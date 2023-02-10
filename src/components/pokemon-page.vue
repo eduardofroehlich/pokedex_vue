66d7b717fed4388ea555ff1a5fdda60aed79bc0b
Eduardo Froehlich <eduardo.froehlich@quantosobra.com.br>
2023-02-10 14:24:48 -0300

    Criar a página principal dos pokemons
    Signed-off-by: Eduardo Froehlich <eduardo.froehlich@quantosobra.com.br>

---
+++ src/components/pokemon-page.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <h1>Pokedex</h1>
+    <PokemonList :imageUrl="imagesUrl" :apiUrl="apiUrl" />
+  </div>
+</template>
+
+<script lang="ts">
+import PokemonList from "./pokemon-list.vue";
+
+export default {
+  data: () => {
+    return {
+      imagesUrl:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/",
+      apiUrl: "https://pokeapi.co/api/v2/pokemon",
+    };
+  },
+  components: {
+    PokemonList,
+  },
+};
+</script>
+
+<style>
+h1 {
+  @apply text-white text-5xl;
+}
+</style>
