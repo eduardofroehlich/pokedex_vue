a37bf3382df629571f46d81e5143c375f57a77ab
Eduardo Froehlich <eduardosilvafroehlich@gmail.com>
2023-02-11 11:39:28 -0300

    Consumir a API para trazer todos os pokemons
    Signed-off-by: Eduardo Froehlich <eduardosilvafroehlich@gmail.com>

--- src/components/pokemon-list.vue
+++ src/components/pokemon-list.vue
@@ -1,61 +1,85 @@
 <template>
   <div class="list">
     <article v-for="(pokemon, index) in pokemons" :key="'poke' + index">
-      <img :src="imageUrl + pokemon.id + '.png'" width="96" height="96" />
+      <img :src="imageUrl + pokemon.id + '.png'" />
       <h3>{{ pokemon.name }}</h3>
     </article>
+    <div class="bg-white" ref="infinitescrolltrigger">
+        <img src="../assets/spinner.svg" width="50" height="50" alt="">
+    </div>
+
   </div>
 </template>
 
 <script lang="ts">
+
 export default {
-  props: ["imageUrl", "apiUrl"],
-  data: () => {
-    return {
-      pokemons: [],
-      nextUrl: "",
-    };
-  },
-  methods: {
-    fetchData() {
-      let req = new Request(this.apiUrl);
-      fetch(req)
-        .then((resp) => {
-          if (resp.status === 200) return resp.json();
-        })
-        .then((data) => {
-          this.nextUrl = data.next;
-          data.results.forEach((pokemon: object) => {
-            pokemon.id = pokemon.url
-              .split("/")
-              .filter(function (part: string) {
-                return !!part;
-              })
-              .pop();
-            this.pokemons.push(pokemon);
-          });
-        })
-        .catch((error) => {
-          console.log(error);
-        });
+    props: ["imageUrl", "apiUrl"],
+    data: () => {
+        return {
+            pokemons: [],
+            nextUrl: "",
+            currentUrl: "",
+        };
     },
-  },
-  created() {
-    this.fetchData();
-  },
+    methods: {
+        fetchData() {
+          let req = new Request(this.currentUrl);
+          fetch(req)
+            .then((resp) => {
+              if (resp.status === 200) return resp.json();
+          })
+            .then((data) => {
+            this.nextUrl = data.next;
+            data.results.forEach((pokemon: object) => {
+              pokemon.id = pokemon.url
+                .split("/")
+                .filter(function (part: string) {
+                  return !!part;
+                })
+                .pop();
+              this.pokemons.push(pokemon);
+            });
+          })
+              .catch((error) => {
+              console.log(error);
+          });
+        },
+        scrollTrigger() {
+          const observer = new IntersectionObserver((entries) => {
+            entries.forEach(entry => {
+              if (entry.intersectionRatio > 0 && this.nextUrl) {
+                this.next();
+              }
+            });
+          });
+          observer.observe(this.$refs.infinitescrolltrigger);
+        },
+        next() {
+            this.currentUrl = this.nextUrl;
+            this.fetchData();
+        },
+    },
+    created() {
+        this.currentUrl = this.apiUrl;
+        this.fetchData();
+    },
+    mounted() {
+        this.scrollTrigger();
+    }
 };
 
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .list {
-  @apply grid gap-2.5;
+  @apply grid gap-2.5 grid-cols-auto;
   @apply w-full max-w-[510px];
 }
 article {
-  @apply h-40 text-center capitalize;
+  @apply grid justify-center h-40 bg-white text-center capitalize;
   @apply rounded-md cursor-pointer;
-  @apply shadow-md;
+  @apply shadow-lg;
 }
 
 h3 {
